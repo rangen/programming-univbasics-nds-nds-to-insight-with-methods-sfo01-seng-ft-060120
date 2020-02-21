d21fd40887fe9fd8c60e4b4713574cf2d61fd99c
Don Mallory <dmm333@gmail.com>
2020-02-21 18:33:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,9 +14,12 @@ end
 
 def directors_totals(nds)
   result = {}
-
-
-
-
+  director_index = 0
+  
+  while director_index < nds.length
+    director = nds[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
   result
 end
